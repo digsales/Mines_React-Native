3520f4d2461174af90dcdc1e632179f88dd1c325
_digsales <diogodobu@gmail.com>
2022-10-29 10:18:48 -0300

    Add Bigger Flag style

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -4,17 +4,19 @@ import { View, StyleSheet } from "react-native";
 export default (props) => {
   return (
     <View style={styles.container}>
-      <View style={styles.flagpole} />
-      <View style={styles.flag} />
-      <View style={styles.base1} />
-      <View style={styles.base2} />
+      <View
+        style={[styles.flagpole, props.bigger ? styles.flagpoleBigger : null]}
+      />
+      <View style={[styles.flag, props.bigger ? styles.flagBigger : null]} />
+      <View style={[styles.base1, props.bigger ? styles.base1Bigger : null]} />
+      <View style={[styles.base2, props.bigger ? styles.base2Bigger : null]} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 2,
+    marginTop: 3,
   },
   flagpole: {
     position: "absolute",
@@ -22,15 +24,13 @@ const styles = StyleSheet.create({
     width: 2,
     backgroundColor: "#222",
     marginLeft: 7,
-    marginTop: 1,
   },
   flag: {
     position: "absolute",
     height: 5,
-    width: 6,
+    width: 7,
     backgroundColor: "#f22",
     marginLeft: 9,
-    marginTop: 1,
   },
   base1: {
     position: "absolute",
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
     width: 6,
     backgroundColor: "#222",
     marginLeft: 5,
-    marginTop: 11,
+    marginTop: 10,
   },
   base2: {
     position: "absolute",
@@ -46,6 +46,28 @@ const styles = StyleSheet.create({
     width: 10,
     backgroundColor: "#222",
     marginLeft: 4,
-    marginTop: 13,
+    marginTop: 12,
+  },
+  flagpoleBigger: {
+    height: 28,
+    width: 4,
+    marginLeft: 14,
+  },
+  flagBigger: {
+    height: 10,
+    width: 14,
+    marginLeft: 18,
+  },
+  base1Bigger: {
+    height: 4,
+    width: 12,
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  base2Bigger: {
+    height: 4,
+    width: 20,
+    marginTop: 24,
+    marginLeft: 8,
   },
 });
